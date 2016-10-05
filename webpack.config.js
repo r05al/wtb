90a551b1bb33d74e0ea18015559700a406d356fd
Peter Rosal <peter.rosal@gmail.com>
2016-10-05 17:36:32 -0400

    Add devserver config and script

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
 module.exports = {
+  devtool: 'eval-source-map',
   entry:  __dirname + "/app/App.js",
   output: {
     path: __dirname + "/public",
     filename: "bundle.js"
+  },
+
+  devServer: {
+    contentBase: "./public",
+    colors: true,
+    historyApiFallback: true,
+    inline: true
   }
 }
