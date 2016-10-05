1e8fe4cbd58b6fdadb763ce645a382a212e74e06
Peter Rosal <peter.rosal@gmail.com>
2016-10-05 17:42:18 -0400

    Add loaders and babel config

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,25 @@ module.exports = {
     filename: "bundle.js"
   },
 
+  module: {
+    loaders: [
+      {
+        test: /\.json$/,
+        loader: "json"
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css'
+      }
+    ]
+  },
+
+
   devServer: {
     contentBase: "./public",
     colors: true,
