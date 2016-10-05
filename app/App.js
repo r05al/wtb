f6960f7621c397c6f4a92f047ef79b2bef3df787
Peter Rosal <peter.rosal@gmail.com>
2016-10-05 17:49:13 -0400

    Add Look component

---
+++ app/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Look from './components/Look';
+
+render(<Look />, document.getElementById('root'));
---
+++ app/components/Look.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Look extends Component {
+  render() {
+    return (
+      <div>
+        "Create your look here"
+      </div>
+    );
+  }
+}
+
+export default Look;
