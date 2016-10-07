66e01e236e0b882ca709cf9618dc4a0e634d18b8
Peter Rosal <peter.rosal@gmail.com>
2016-10-07 14:38:46 -0400

    Add deselect cb and proptypes for Look

--- app/components/Look.js
+++ app/components/Look.js
@@ -1,18 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import ClothingItem from './ClothingItem';
 
 class Look extends Component {
   render() {
     let look = this.props.look.map((item) => {
-    	return <ClothingItem key={item.id} {...item} />
+    	return <div className="look-grid" key={item.id}
+									onClick={ this.props.lookCallbacks.deselect.bind(null, item) }>
+						 	{item.name}
+						 </div>
     });
     return (
-      <div>
+      <div className="look">
         {this.props.title}
         { look }
       </div>
     );
   }
+}
+
+Look.propTypes = {
+	title: PropTypes.string,
+	description: PropTypes.string,
+	date: PropTypes.object,
+	look: PropTypes.arrayOf(PropTypes.object),
+	lookCallbacks: PropTypes.object
 }
 
 export default Look;
