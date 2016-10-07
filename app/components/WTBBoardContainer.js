ddf81d5fcfda0fe97fa569424f202019b125e76e
Peter Rosal <peter.rosal@gmail.com>
2016-10-07 14:41:22 -0400

    Add addtional state items and cb fns

--- app/components/WTBBoardContainer.js
+++ app/components/WTBBoardContainer.js
@@ -1,26 +1,69 @@
 import React, { Component } from 'react';
 import items from '../../public/items.json';
 import WTBBoard from './WTBBoard';
+import update from 'react-addons-update';
+import moment from 'moment';
 
 class WTBBoardContainer extends Component {
   constructor() {
     super(...arguments);
     this.state = {
       clothingItems: [],
-      look: []
+      look: [],
+      selectedDate: moment(),
+      savedLooks: []
     };
   }
 
   componentDidMount() {
     this.setState({ clothingItems: items });
-    this.setState({ look: [{'name':"barbour", "id": 1},{'name':"gitman", "id": 2},
-                           {'name':"levis", "id": 3},{'name':"vans", "id": 4}] });
+    this.setState({ look: [{'name':"barbour", "id": 1, "type": "jacket"},
+                           {'name':"gitman", "id": 2, "type": "shirt"},
+                           {'name':"levis", "id": 3, "type": "pant"},
+                           {'name':"vans", "id": 4, "type": "shoe"}] });
+  }
+
+  select(item) {
+    let lookIndex = this.state.look.findIndex((piece) => piece.type == item.type);
+
+    let nextState = update(
+      this.state.look, {
+        [lookIndex]: { $set: item }
+      }
+    );
+
+    this.setState({look: nextState});
+  }
+
+  deselect(item) {
+    let lookIndex = this.state.look.findIndex((piece) => piece.type == item.type);
+
+    let nextState = update(
+      this.state.look, {
+        [lookIndex]: { $set: {"type": item.type } }
+      }
+    );
+
+    this.setState({look: nextState});
+
+  }
+
+  handleChange(date) {
+    this.setState({selectedDate: date});
   }
 
   render() {
     return (
       <WTBBoard look={this.state.look}
-                clothingItems={this.state.clothingItems} />
+                clothingItems={this.state.clothingItems}
+                selectedDate={this.state.selectedDate}
+                lookCallbacks={
+                                { 
+                                  select: this.select.bind(this),
+                                  deselect: this.deselect.bind(this),
+                                  handleChange: this.handleChange.bind(this)
+                                }
+                              } />
     )
   }
 }
