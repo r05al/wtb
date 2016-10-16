5d74327dc3ff8632334df557040b702b19a325e7
Peter Rosal <peter.rosal@gmail.com>
2016-10-15 23:04:26 -0400

    Customize slider buttons, restyle slider images, add animation

--- app/components/List.js
+++ app/components/List.js
@@ -1,6 +1,35 @@
 import React, { Component, PropTypes } from 'react';
 import Slider from 'react-slick';
 import { Link } from 'react-router';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
+class RightNavButton extends Component {
+  render() {
+			let style = {
+			left: 0,
+			position: "absolute",
+			zIndex: 1,
+			top: "45%",
+			opacity: ".7",
+			cursor: "pointer"
+		}
+    return <span{...this.props} style={style}>←</span>  
+  }
+}
+
+class LeftNavButton extends Component {
+  render() {
+		let style = {
+			right: 0,
+			position: "absolute",
+			zIndex: 1,
+			top: "45%",
+			opacity: ".7",
+			cursor: "pointer"
+		}
+    return <span{...this.props} style={style}>→</span>  
+  }
+}
 
 class List extends Component {
 	constructor() {
@@ -16,11 +45,15 @@ class List extends Component {
 	}
 
   render() {
+  	let leftNav = <LeftNavButton />;
+  	let rightNav = <RightNavButton />;
 		let settings = {
 			infinite: true,
 			speed: 500,
 			slidesToShow: 7,
-			slidesToScroll: 1
+			slidesToScroll: 1,
+			nextArrow: leftNav,
+			prevArrow: rightNav
 		};
 
   	let clothingItems;
@@ -29,31 +62,34 @@ class List extends Component {
 	  		return (
 	  			<div className='clothing' key={item.id} onClick={
 	  				this.props.lookCallbacks.select.bind(null, item) }>
-	  				<div className="item-edit"><Link to={'/edit/'+item.id}>✎</Link></div>
-	  				<img src={ item.href } />
+	  				<div style={{width: "90%", margin: "0 auto"}}>
+		  				<div className="item-edit"><Link to={`items/${item.id}/edit`}>✎</Link></div>
+		  				<img src={ item.href } />
+		  			</div>
 	  			</div>
 	  		);
 	  	});
 	 		var slider = <Slider {...settings}>{clothingItems}</Slider>
   	}
 
-
-
     return (
-      <div className={ this.state.showItems ? "list list--is-open" : "list" }>
-      	<div className={
-      		this.state.showItems ? "list-title list-title--is-open" : "list-title"
-      		} onClick={this.toggleItems.bind(this)}>
-      		{this.props.title}
-      	</div>
-    		{ slider }
-      </div>
+    	<div className="list">
+	    	<div className={ this.state.showItems ? 
+    					`list-title list-title--is-open ${this.props.id}` : 
+    					`list-title ${this.props.id}`} 
+    					onClick={this.toggleItems.bind(this)}>
+	      </div>
+    		<ReactCSSTransitionGroup transitionName="toggle"
+    		                         transitionEnterTimeout={500}
+    		                     		 transitionLeaveTimeout={500}>
+		  	{ slider }
+		  	</ReactCSSTransitionGroup>
+	  	</div>
     );
   }
 }
 
 List.propTypes = {
-	title: PropTypes.string.isRequired,
 	clothingItems: PropTypes.arrayOf(PropTypes.object),
 	lookCallbacks: PropTypes.object
 };
